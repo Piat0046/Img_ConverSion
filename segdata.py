25a1eaa64178b4528afc842658ba98d60775741c
git <github.com-Piat0046>
2022-08-18 01:43:24 +0000

    make utils.py

--- segdata.py
+++ segdata.py
@@ -2,84 +2,104 @@ import os
 import cv2
 import json
 import numpy as np
-from utils import createFolder
+from PIL import Image
+from tqdm import tqdm
 from pprint import pprint
-
-# Make Save Folder
-mode = 'vaild'
-save_path = '/home/ubuntu/fire/aihub/dataset'
-folder_list = [mode, mode+'/Annotations', mode+'/JPEGImages']
-jpg_save_path = folder_list[2]
-mask_save_path = folder_list[1]
-folder_path_list = [os.path.join(save_path, folder) for folder in folder_list]
-for folder_path in folder_path_list: createFolder(folder_path)
+from multiprocessing import Pool
+from os.path import join as path_join
+from utils import createFolder, convert_cmap, check_color
 
 
-path = '/home/ubuntu/fire/aihub/Validation'
+def main(name):
+    try:
+        global path
+        global mask_save_path
+        global jpg_save_path
 
-img_list = os.listdir(path)
+        # load files & folder
+        img_path = os.path.join(path, 'img')
+        json_path = os.path.join(path, 'label')
+        img_file = os.path.join(img_path, name+'.jpg')
+        json_file = os.path.join(json_path, name+'.json')
+        img = cv2.imread(img_file, cv2.IMREAD_COLOR)
+        with open(json_file, 'r', encoding='utf-8-sig') as f:
+            json_dic = json.load(f)
+        
+        # make poly mask
+        img_w, img_h = img.shape[0], img.shape[1]
+        annotations = json_dic['annotations']
+        mask_zero = np.zeros((img_w, img_h,3), dtype = np.uint8)
+        #"license": 1,"file_name": "COCO_test2014_000000027454.jpg","coco_url": "http://images.cocodataset.org/test2014/COCO_test2014_000000027454.jpg","height": 640,"width": 427,"date_captured": "2013-11-22 01:57:00","id": 27454}
+        for num in range(len(annotations)):
+            class_num = annotations[num]['class']
+            color = check_color(class_num)
+            if 'polygon' in list(annotations[num].keys()):
+                polygon = json_dic['annotations'][num]['polygon']
 
-def convert_cmap(mask_img):
-    cmap = [[0,0,0],[250,255,100]]
-    palette = [0,0,0,250,255,100]
+            else:
+                x, y, w, h = json_dic['annotations'][num]['box']
+                polygon = [[x,y],[w, y], [w, h], [x, h]]
+            print(polygon)
+            print(img_w, img_h)
+            poly_mask = np.array(polygon, np.int32)
+            cv2.fillPoly(mask_zero, [poly_mask], color)
+       
+        # write json data
+        img_meta = {"license": 1,
+                     "file_name": img_file,
+                     "coco_url": "http://images.cocodataset.org/test2014/COCO_test2014_000000027454.jpg",
+                     "height": img_w, "width": img_h,
+                     "date_captured": json_dic['image']['date']
+                    }
 
-    img_png = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+        mask = cv2.cvtColor(mask_zero, cv2.COLOR_BGR2RGB)
+        img_png = convert_cmap(mask, img_w, img_h)
 
-    for index, val_col in enumerate(cmap):
-        img_png[np.where(np.all(mask_img == val_col, axis=-1))] = index
+        # Save img
+        #mask_path = path_join(mask_save_path, name[:-5])
+        #jpg_path = path_join(jpg_save_path, name[:-5])
 
-    img_png = Image.fromarray(img_png).convert('P')
-    # Palette information injection
-    img_png.putpalette(palette)
+        img_png.save(path_join(mask_save_path, f'{name}.png'))
+        cv2.imwrite(path_join(jpg_save_path, f'{name}.jpg'), img)
 
-    return img_png
+        return img_meta
+            
+    except Exception as e:
+        print(e)
 
 
-def main(img):
-    global path
 
-    # load files
-    name = img
-    img_path = os.path.join(path, 'img')
-    json_path = os.path.join(path, 'label')
-    img_file = os.path.join(img_path, img+'.jpg')
-    json_file = os.path.join(json_path, img+'.json')
-    img = cv2.imread(img_file, cv2.IMREAD_COLOR)
-    with open(json_file, 'r', encoding='utf-8-sig') as f:
-        json_dic = json.load(f)
+if __name__=='__main__':
+
+    path = '/home/ubuntu/fire/aihub/Validation'
+    jpg_path = os.path.join(path, 'img')
+    img_list = [i.rsplit('.')[0] for i in os.listdir(jpg_path)]
+
+    # Make Folder
+    mode = 'valid'
+    save_path = '/home/ubuntu/fire/aihub/dataset'
+    folder_list = [mode, mode+'/Annotations', mode+'/JPEGImages']
+    #folder_name = set([i[:-5] for i in img_list])
+    #for i in folder_name: folder_list.append(path_join(folder_list[1], i)), folder_list.append(path_join(folder_list[2], i))
+    folder_path_list = [os.path.join(save_path, folder) for folder in folder_list]
+    jpg_save_path = folder_path_list[2]
+    mask_save_path = folder_path_list[1]
+    for folder_path in folder_path_list: createFolder(folder_path)
+
+    make_json = {"info": {"description": "COCO 2014 Dataset","url": "http://cocodataset.org","version": "1.0","year": 2014,"contributor": "COCO Consortium","date_created": "2017/09/01"},
+                 "images" : [],
+                 "licenses": [{"url": "http://creativecommons.org/licenses/by-nc-sa/2.0/","id": 1,"name": "Attribution-NonCommercial-ShareAlike License"},{"url": "http://creativecommons.org/licenses/by-nc/2.0/","id": 2,"name": "Attribution-NonCommercial License"},{"url": "http://creativecommons.org/licenses/by-nc-nd/2.0/","id": 3,"name": "Attribution-NonCommercial-NoDerivs License"},{"url": "http://creativecommons.org/licenses/by/2.0/","id": 4,"name": "Attribution License"},{"url": "http://creativecommons.org/licenses/by-sa/2.0/","id": 5,"name": "Attribution-ShareAlike License"},{"url": "http://creativecommons.org/licenses/by-nd/2.0/","id": 6,"name": "Attribution-NoDerivs License"},{"url": "http://flickr.com/commons/usage/","id": 7,"name": "No known copyright restrictions"},{"url": "http://www.usa.gov/copyright.shtml","id": 8,"name": "United States Government Work"}],
+                 "categories" : [{"supercategory": "fire","id": 1,"name": "black_smoke"},{"supercategory": "fire","id": 2,"name": "gray_smoke"},{"supercategory": "fire","id": 3,"name": "white_smoke"},{"supercategory": "fire","id": 4,"name": "fire"}]
+                 }
+
+
+    cpu = 16
+    with Pool(cpu) as pool:
+        for num, i in enumerate(tqdm(pool.imap_unordered(main, img_list[:1]))):
+            i['id'] = num
+            make_json['images'].append(i)
     
-    # make poly mask
-    img_w, img_h = img.shape[0], img.shape[1]
-    polygon = json_dic['annotations'][0]['polygon']
+    json_path = path_join(path_join(save_path, mode), mode+'.json')
 
-    color = [100,255,250] # yellow
-    mask_zero = np.zeros((img_w, img_h,3), dtype = np.uint8)
-    poly_mask = np.array(polygon, np.int32)
-    cv2.fillPoly(mask_zero, [poly_mask], color)
-    mask = cv2.cvtColor(mask_zero, cv2.COLOR_BGR2RGB)
-    img_png = convert_cmap(mask)
-
-    img_png.save(os.path.join(mask_path, f'{file_name}_{num}.png'))
-    cv2.imwrite(os.path.join(jpg_path, f'{file_name}_{num}.jpg'), img)
-    # train_txt.write(f'{file_name}_{num}\n')
-
-
-    return 
-
-img_2 = 'S3-N1215MF00644.jpg'
-
-
-pprint(main(img_2.rsplit('.')[0]))
-
-# if __name__=='__main__':
-
-#     path = r'/home/ubuntu/Cardata/scratch'
-#     with open('./labels/train2.txt', 'w', encoding='UTF=8') as f:
-#         f.close()
-
-#     all_files_list = os.listdir(path)
-#     files_list = [i for i in all_files_list if 'png' not in i and 'tif' not in i]
-#     cpu = 16
-#     with Pool(cpu) as pool:
-#         for _ in tqdm(pool.imap_unordered(main, files_list)):
-#             pass
+    with open(json_path, "w") as json_file:
+        json.dump(make_json, json_file)
--- utils.py
+++ utils.py
@@ -1,10 +1,77 @@
 import os
+import numpy as np
+from PIL import Image
 
 def createFolder(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
-        else:
-            print('Already directory exist')
     except OSError:
         print('Error: Creating directory. '+ directory)
+
+def convert_cmap(mask_img, img_w, img_h ):
+    # 01: black smoke [0,15,255]
+    # 02: gray smoke [130,130,130]
+    # 03: white smoke [255,255,255]
+    # 04: fire smoke [255,0,0]
+    cmap = [[0,0,0],[0,15,255],[130,130,130],[255,255,255],[255,0,0]]
+    palette = [0,0,0,0,15,255,130,130,130,255,255,255,255,0,0]
+
+    img_png = np.zeros((img_w, img_h), np.uint8)
+
+    for index, val_col in enumerate(cmap):
+        img_png[np.where(np.all(mask_img == val_col, axis=-1))] = index
+
+    img_png = Image.fromarray(img_png).convert('P')
+    # Palette information injection
+    img_png.putpalette(palette)
+
+    return img_png
+
+def make_json(name, dic, path):
+    json_path = os.path.join(path, name+'json')
+    with open(json_path, "w") as json_file:
+        json.dump(dic, json_file)
+
+
+
+def check_color(text):
+    class_num = text
+    if class_num == '01':
+        return [255,15,0]
+    elif class_num == '02':
+        return [130,130,130]
+    elif class_num == '03':
+        return [255,255,255]
+    elif class_num == '04':
+        return [0,0,255]
+    
+# Fast run length encoding
+def rle (img):
+    flat_img = img.flatten()
+    flat_img = np.where(flat_img > 0.5, 1, 0).astype(np.uint8)
+
+    starts = np.array((flat_img[:-1] == 0) & (flat_img[1:] == 1))
+    ends = np.array((flat_img[:-1] == 1) & (flat_img[1:] == 0))
+    starts_ix = np.where(starts)[0] + 2
+    ends_ix = np.where(ends)[0] + 2
+    lengths = ends_ix - starts_ix
+    
+    return starts_ix, lengths
+
+
+from detectron2.structures import polygons_to_bitmask
+def polygon_to_rle(polygon: list, shape=(1920, 1080)):
+    '''
+    polygon: a list of [x1, y1, x2, y2,....]
+    shape: shape of bitmask
+    Return: RLE type of mask
+    '''
+    mask = polygons_to_bitmask([np.asarray(polygon) + 0.25], shape[0], shape[1]) # add 0.25 can keep the pixels before and after the conversion unchanged
+    rle = mask_util.encode(np.asfortranarray(mask))
+    return rle
+
+polygon = [[757, 39], [725, 51], [702, 59], [679, 66], [628, 98], [589, 121], [558, 128], [521, 159], [513, 185], [505, 214], [502, 235], [498, 262], [509, 296], [537, 318], [562, 339], [576, 347], [596, 352], [626, 356], [632, 367], [636, 392], [654, 415], [655, 437], [657, 461], [657, 487], [665, 504], [680, 515], [700, 541], [703, 570], [715, 595], [721, 611], [733, 636], [746, 652], [742, 670], [731, 687], [720, 710], [718, 730], [724, 747], [728, 766], [731, 784], [735, 803], [739, 821], [740, 843], [754, 862], [765, 878], [773, 898], [781, 908], [792, 919], [820, 921], [854, 921], [881, 919], [918, 918], [947, 919], [980, 921], [1011, 921], [1031, 918], [1059, 895], [1065, 877], [1065, 856], [1062, 835], [1055, 817], [1047, 803], [1046, 780], [1047, 754], [1050, 733], [1050, 711], [1047, 689], [1042, 652], [1035, 613], [1028, 584], [1028, 551], [1028, 521], [1033, 491], [1046, 461], [1063, 432], [1073, 417], [1076, 403], [1083, 381], [1091, 367], [1098, 351], [1099, 339], [1092, 332], [1088, 315], [1087, 289], [1085, 273], [1085, 254], [1087, 236], [1102, 217], [1115, 199], [1135, 177], [1137, 169], [1140, 152], [1140, 139], [1137, 100], [1128, 73], [1122, 58], [1111, 43], [1088, 30], [1072, 24], [1048, 21], [1028, 14], [1005, 14], [983, 14], [966, 15], [942, 24], [925, 43], [907, 58], [895, 65], [884, 63], [881, 48], [877, 33], [865, 22], [831, 18], [811, 18], [788, 25]]
+poly = []
+for i in polygon:  poly.extend(i)
+polygon_to_rle(poly)
